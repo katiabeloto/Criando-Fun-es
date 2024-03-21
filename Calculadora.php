dcfd257f30c8dcef6ca698d619c24805e86507d1
katiabeloto <katia.beloto@hotmail.com>
2024-03-20 22:16:34 -0300

    Criando Funções

---
+++ Calculadora.php
@@ -0,0 +1,19 @@
+<?php 
+
+$num1=20.50;
+$num2=30.50;
+
+function somar($num1,$num2){
+    	return($num1+$num2);
+}
+function subtrair($num1,$num2){
+    	return($num1-$num2);
+}
+function multiplicar($num1,$num2){
+    	return($num1*$num2);
+}
+function dividir($num1,$num2){
+    	return($num1/$num2);
+}
+
+ ?>
---
+++ exemplo09.php
@@ -0,0 +1,10 @@
+<?php 
+
+function calcular($num1, $num2){
+
+		return(20.50+30.50);
+}
+
+	echo calcular(20.50,30.50);	
+
+ ?>
